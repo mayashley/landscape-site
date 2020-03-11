15893553403398c3942bd853209f143aacf4fc5a
mayashley052@gmail.com <mayashley052@gmail.com>
2020-03-11 17:33:37 -0400

    footer adjustment

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from "react"
-import theLinks from "../constants/Links"
-import { Link } from "gatsby"
+// import theLinks from "../constants/Links"
+// import { Link } from "gatsby"
 import styles from "../css/footer.module.css"
 import SocialIcons from "../constants/FooterIcons"
 import { Example} from "./SocialIcons"
@@ -8,7 +8,7 @@ import { Example} from "./SocialIcons"
 const Footer = () => {
   return (
     <footer className={styles.footerWrapper}>
-      <div className={styles.theLinks}>
+      {/* <div className={styles.theLinks}>
         {theLinks.map((item, index) => {
           return (
             <Link key={index} to={item.path}>
@@ -16,7 +16,7 @@ const Footer = () => {
             </Link>
           )
         })}
-      </div>
+      </div> */}
       <div className={styles.SocialIconsWrapper}>
         {SocialIcons.map((item, index) => {
           return <Example data={item} key={index} />
