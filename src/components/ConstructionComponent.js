4416d4bc0bc1c5e810ab03858244bf2034bd9cfd
mayashley052@gmail.com <mayashley052@gmail.com>
2020-03-13 12:46:02 -0400

    working on css for the construction component.

--- src/components/ConstructionComponent.js
+++ src/components/ConstructionComponent.js
@@ -1,25 +1,27 @@
-import React from 'react'
+import React from "react"
 
-import Banner from '../components/Banner'
-import constructionImg from '../images/constructionImg.png'
-import styles from '../css/underConstruction.module.css'
+import Banner from "../components/Banner"
+import constructionImg from "../images/constructionImg.png"
+import styles from "../css/underConstruction.module.css"
 
-
-
- const ConstructionComponent = () => {
-    return (
-        
-            <div>
-           <Banner
-          title="Under Construction"
-          info="Please be patient with us while our business grows.We Hope that you will vist this page again soon!"
+const ConstructionComponent = () => {
+  return (
+    <div className={styles.constructionPageWrapper}>
+      <div className={styles.Banner}>
+        <Banner
+          title="Under Construction" info="Please be patient with us while our
+          business grows.We Hope that you will vist this page again soon!"
         ></Banner>
-        <img src={constructionImg} alt='page under construction'/>
-        <div>
-        <button type="button" className="btn-primary">Go Back</button>
-        </div>
-        </div>
-        
-    )
-    }
+      </div>
+      <div className={styles.imageContainer}>
+      <img src={constructionImg} alt="page under construction" />
+      </div>
+      <div className={styles.buttonWrapper}>
+        <button type="button" className="btn-primary">
+          Go Back
+        </button>
+      </div>
+    </div>
+  )
+}
 export default ConstructionComponent
